8e2453df139db38c169e2b904923f087efc44c84
William Skaggs <wskaggs3262@gmail.com>
2021-10-20 20:50:54 -0500

    Create compatibility route to find the compatibility a user has with some other user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -99,3 +99,77 @@ exports.findByPoemId = (req, res) => {
                 .send({ message: err.message || "Error retrieving Poem with id=" + id });
         });
 }
+
+exports.getCompatibility = (req, res) => {
+    const conditions = req.body.conditions;
+    const allPoemTags = [];
+    const username = req.params.username;
+
+    if (!username || !conditions) {
+        res.status(400).send({ message: "Contents cannot be empty."});
+        return;
+    }
+
+    User.findOne({ username: username })
+        .then(user => {
+            if(!user) {
+                res.status(404).send({ message: "Did not find User with username " + username });
+            } else {
+                console.log(user.poems);
+                const poems = user.poems;
+                Poem.find({ title: { $in: poems } })
+                    .then(data => {
+                        console.log(data);
+                        data.forEach(poem => poem.tags.forEach(tag => allPoemTags.push(tag)));
+                        res.send({ "score": compatibilityScore(req.body.user, user, allPoemTags, conditions) });
+                    })
+                    .catch(err => {
+                        res.status(500).send({
+                            message: err.message || "Error retrieving poems by username."
+                        });
+                    })
+            }
+
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Error retrieving user by username."
+            });
+        });
+}
+
+function compatibilityScore(user, potentialUser, allTags, conditions) {
+    let compatibility = 0;
+    compatibility += proximityValue(user.poems.length, potentialUser.poems.length, conditions.proximityWeight);
+    compatibility += matchingTags(user, allTags);
+    return compatibility;
+}
+
+/**
+ * Calculates a score value based on the difference in the number of poems between the user and potential match
+ * @param {int} userPoemCount The user's number of poems.
+ * @param {int} potentialMatchPoemCount The potential match's number of poems.
+ * @param {number} weight The weight of the value, positive if you want the match to have more poems than you,
+ *                                                 negative if you want the match to have fewer poems than you.
+ * @returns {number} The score.
+ */
+function proximityValue(userPoemCount, potentialMatchPoemCount, weight) {
+    return potentialMatchPoemCount > userPoemCount ? getBaseLog(2, Math.abs(potentialMatchPoemCount - userPoemCount)) * weight :
+        potentialMatchPoemCount === userPoemCount ? 0 : getBaseLog(2, Math.abs(potentialMatchPoemCount - userPoemCount)) * -weight;
+}
+
+/**
+ * Calculates a score value based on the number of matching tags between the user's personal tags and a list of poem tags.
+ * @param {User} user The user to test against.
+ * @param {string[]} allTags The list of tags (possibly contains duplicates).
+ * @returns {number} The score.
+ */
+function matchingTags(user, allTags) {
+    let tagCount = 0;
+    allTags.forEach(tag => tagCount += user.tags.includes(tag) ? 1 : 0);
+    return tagCount;
+}
+
+function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+}
