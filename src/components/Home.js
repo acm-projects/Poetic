e058510d67c6af96db9bde7d6e1ef2facd246cd2
kevinmstapleton <kevinmstapleton@gmail.com>
2021-10-25 20:04:06 -0500

    added sign up and changed main a bit to compensate for new login box

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,49 @@
 import React, { useMemo, useState } from 'react'
 import PoemScroll from './PoemScroll';
+import axios from 'axios';
 
-const Home = () => {
+export default function Home() {
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+    });
+    const signupRoute = 'http://localhost:8080/api/authentication/register'
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); //prevents screen from reloading
+
+        const { username, password } = values;
+        const user = {username: username, password: password};
+
+        axios.post(signupRoute, user)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    const handleChange = name => e => {
+        setValues({ ...values, [name]: e.target.value });
+    };
+
     return (
-        <div class="flex gap-8 p-4 bg-red-200 shadow-inner">
-            <div class="text-6xl rounded bg-white p-4 shadow">
+        <div class="flex gap-4 p-4 bg-red-400 shadow-inner">
+            <div class="text-6xl rounded bg-white p-4 shadow w-1/5">
                 Hello
+                <div className="self-start flex flex-col gap-4 shadow-md rounded font-bold p-4 bg-red-400">
+                <h2>Sign Up</h2>
+                <form className="flex flex-col gap-4 ">
+                    <input className="rounded border border-white" type="username" placeholder="Name" class = "text-base"
+                           onChange={handleChange('username')}/>
+                    <input className="rounded" type="password" placeholder="Password" class = "text-base"
+                           onChange={handleChange('password')}/>
+                    <button onClick={handleSubmit} type='submit'
+                    className="text-4xl bg-red-900 text-white rounded focus:outline-black">Submit
+                    </button>
+                </form>
+            </div>
             </div>
             <div class="flex flex-col flex-1">
                 <div class="bg-blue-100 rounded p-2">
@@ -20,7 +58,5 @@ const Home = () => {
                 <PoemScroll />
             </div>
         </div>
-    )
+    );
 }
-
-export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,60 @@
 import React, { useState } from 'react';
-import Poem from './Poem';
-import Form from 'semantic-ui-react'
 import PoemScroll from './PoemScroll';
 import axios from 'axios';
 
 export default function Login () {
-    const [username, setUserName] = useState('');
-    const [password, setPassword] = useState(false);
-    const client = 'https://localhost:8080/api/authentication/login'
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+    });
+    const loginRoute = 'http://localhost:8080/api/authentication/login'
 
-    const postData = () => {
-        console.log("reached post");
-        axios.post(client, {
-            username,
-            password
-        })
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault(); //prevents screen from reloading
+
+        const { username, password } = values;
+        const user = {username: username, password: password};
+
+        axios.post(loginRoute, user)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    const handleChange = name => e => {
+        setValues({ ...values, [name]: e.target.value });
+    };
+
     return (
-        <div class="shadow-inner flex p-4 gap-10 justify-between bg-red-200">
-            <div class="self-start flex flex-col gap-4 shadow-md rounded font-bold p-4 bg-red-400">
+        <div className="shadow-inner flex p-4 gap-10 justify-between bg-red-200">
+            <div className="self-start flex flex-col gap-4 shadow-md rounded font-bold p-4 bg-red-400">
                 <h2>Login</h2>
-                <form class="flex flex-col gap-4">
-                    <input class="rounded border border-white" type="username" placeholder="Name" onChange={(e) => setUserName(e.target.value)}/>
-                    <input class="rounded" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                    <button onClick={postData} type = 'submit' class="bg-red-900 text-white rounded focus:outline-black">Submit</button>
+                <form className="flex flex-col gap-4">
+                    <input className="rounded border border-white" type="username" placeholder="Name"
+                           onChange={handleChange('username')}/>
+                    <input className="rounded" type="password" placeholder="Password"
+                           onChange={handleChange('password')}/>
+                    <button onClick={handleSubmit} type='submit'
+                            className="bg-red-900 text-white rounded focus:outline-black">Submit
+                    </button>
                 </form>
             </div>
-            <div class="self-start flex flex-col gap-4 p-6 font-bold text-8xl flex-1 bg-white rounded">
+            <div className="self-start flex flex-col gap-4 p-6 font-bold text-8xl flex-1 bg-white rounded">
                 <div>
                 Hello
                 </div>
-                <div class="bg-red-50 rounded p-4 font-normal text-6xl">
+                <div className="bg-red-50 rounded p-4 font-normal text-6xl">
                     Here are some words.
                 </div>
-                <div class="text-left text-4xl font-light">
+                <div className="text-left text-4xl font-light">
                     &emsp;These words describe what poetic is blah blah blah
                 </div>
             </div>
-            
-            <PoemScroll />
+
+            <PoemScroll/>
         </div>
-        );
+    );
 }
