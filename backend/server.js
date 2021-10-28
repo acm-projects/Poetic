0d248c821ee529596637d06072448084824d714e
William Skaggs <wskaggs3262@gmail.com>
2021-10-27 21:49:59 -0500

    Move all the frontend stuff into a folder called frontend

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/authentication.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
