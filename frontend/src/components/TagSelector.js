d487a30861758d22ee172ac90bc9966c07e3c4ad
Kevin Stapleton <kevinmstapleton@gmail.com>
2021-11-17 13:51:04 -0600

    changed the placement of the tag selector on the doc editor and made small change in poem display

--- frontend/src/components/TagSelector.js
+++ frontend/src/components/TagSelector.js
@@ -5,9 +5,10 @@ import { TextField } from "@mui/material";
 const TagSelector = (props) => {
 
     return (
-        <div>
+        <div class= "p-10 gap-2 transition-all hover:scale-105">
 <Autocomplete
         multiple
+        position='absolute'
         id="tags-outlined"
         options={["Sonnet", "Haiku", "Limmerick", "Rhyming", "Sad", "Happy", "Imagery", "Free Verse", "Nature"]}
         onChange={(event, value) => props.handleTagChange(event, value)}
